dce666187feb2dcae224064aaac85bc6ef718107
qonetwo <6b6963686576@gmail.com>
2020-05-04 22:39:46 +0300

    Init project by spring.start.io.

---
+++ src/test/kotlin/com/tryhev/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tryhev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
